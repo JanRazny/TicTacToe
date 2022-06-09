3281f48f6c206d0d98a969babf62cf3941bd7d83
Jan Razny <netorg@gmx.de>
2022-06-09 21:17:25 +0200

    grafik

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-function init() {
-    console.log('test');
+let fields = [];
+
+let currentShape = 'cross';
+
+function fillShape(id) {
+    if (currentShape == 'cross') {
+        currentShape = 'circle';   
+    } else {
+        currentShape = 'cross';
+    }
+
+
+    fields[id] = currentShape;
+    console.log(fields);
+    draw();
+}
+
+function draw() {
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i] == 'circle') {
+            document.getElementById('circle-' + i).classList.remove('d-none');    
+        }
+
+        if (fields[i] == 'cross') {
+            document.getElementById('cross-' + i).classList.remove('d-none');    
+        }
+
+
+        
+    }
 }
